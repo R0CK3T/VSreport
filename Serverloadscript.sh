af12750d2ea002bfe721272ac74be79eebb36b73
R0CK3T <elarchaouimed@gmail.com>
2021-05-25 21:07:32 +0200

    Update Serverloadscript.sh

--- Serverloadscript.sh
+++ Serverloadscript.sh
@@ -4,6 +4,6 @@ tag=$(whiptail --inputbox "Tag :" 10 30 3>&1 1>&2 2>&3)
 serialnumb=`sudo dmidecode -t system | grep Serial  | awk -F ': ' '{ print $2 }'`
 datentime=`date +'%d-%m-%Y_%H-%M-%S'`
 sudo lshw -html > /root/$tag"_"$serialnumb"_"$datentime.html
-#edit ip.add.re.ss and  reporteruser,reporteruserpassword to ass your login 
+#edit ip.add.re.ss and  reporteruser,reporteruserpassword to pass your login credentials
 lftp -e "put /root/$tag"_"$serialnumb"_"$datentime.html; quit" sftp://ip.add.re.ss -p 22 -u reporteruser,reporteruserpassword
 echo "Done sending..."
