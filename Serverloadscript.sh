49a7bd5d9a6ea635c06290abebeee647b79f0a64
R0CK3T <elarchaouimed@gmail.com>
2021-05-25 12:24:41 +0200

    Create Serverloadscript.sh

---
+++ Serverloadscript.sh
@@ -0,0 +1,9 @@
+
+echo "Hello from remote server"
+tag=$(whiptail --inputbox "Tag :" 10 30 3>&1 1>&2 2>&3)
+serialnumb=`sudo dmidecode -t system | grep Serial  | awk -F ': ' '{ print $2 }'`
+datentime=`date +'%d-%m-%Y_%H-%M-%S'`
+sudo lshw -html > /root/$tag"_"$serialnumb"_"$datentime.html
+#edit ip.add.re.ss and  reporteruser,reporteruserpassword to ass your login 
+lftp -e "put /root/$tag"_"$serialnumb"_"$datentime.html; quit" sftp://ip.add.re.ss -p 22 -u reporteruser,reporteruserpassword
+echo "Done sending..."
