e523f1f2c2d63bc9335fbfeb3c2dd8fe43e1d77d
Massipou <84479347+Massipou@users.noreply.github.com>
2021-05-31 22:55:47 +0200

    Update Serverloadscript.sh
    proposition change for bit more flexibility

--- Serverloadscript.sh
+++ Serverloadscript.sh
@@ -1,9 +1,21 @@
-
 echo "Hello from remote server"
 tag=$(whiptail --inputbox "Tag :" 10 30 3>&1 1>&2 2>&3)
 serialnumb=`sudo dmidecode -t system | grep Serial  | awk -F ': ' '{ print $2 }'`
 datentime=`date +'%d-%m-%Y_%H-%M-%S'`
-sudo lshw -html > /root/$tag"_"$serialnumb"_"$datentime.html
-#edit ip.add.re.ss and  reporteruser,reporteruserpassword to pass your login credentials
-lftp -e "put /root/$tag"_"$serialnumb"_"$datentime.html; quit" sftp://ip.add.re.ss -p 22 -u reporteruser,reporteruserpassword
-echo "Done sending..."
+mkdir /root/reports
+if [ "$serialnumb" = "Not Specified" ]
+then
+        reportname=$tag"_unknow_"$datentime.html
+else
+        reportname=$tag"_"$serialnumb"_"$datentime.html
+fi
+sudo lshw -html > /root/reports/$reportname
+#edit ip.add.re.ss and  reporteruser,reporteruserpassword to add your login 
+lftp -e "put /root/reports/$reportname; quit" -u "reporteruser","reporteruserpassword"  sftp://ip.add.re.ss
+if [ "$?" = "0" ]
+then
+        echo "done sending !"
+else
+        echo "fail sending ..."
+fi
+
