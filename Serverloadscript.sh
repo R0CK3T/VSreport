13d76322e1813df4f22d27715259db3bca26d199
R0CK3T <elarchaouimed@gmail.com>
2021-06-17 14:26:33 +0200

    commented report script added

--- Serverloadscript.sh
+++ Serverloadscript.sh
@@ -9,6 +9,26 @@ then
 else
         reportname=$tag"_"$serialnumb"_"$datentime.html
 fi
+
+#echo cpu
+#cat /proc/cpuinfo | grep -m 1  "model name"
+#echo
+#echo memory
+#sudo dmidecode --type 17 | cut -c 2- | grep ^"Size: "
+#sudo dmidecode --type 17 | cut -c 2- | grep ^"Type:"
+#sudo dmidecode --type 17 | cut -c 2- | grep ^"Locator: "
+#echo
+#echo disks
+#sudo nvme list -o=json | tr -d '"' | tr -d ','| grep ModelNumber | cut -c 7-
+#sudo nvme list -o=json | tr -d '"' | tr -d ','| grep SerialNumber | cut -c 7-
+#sudo nvme list -o=json | tr -d '"' | tr -d ','| grep PhysicalSize | cut -c 7-
+#echo
+#echo battery
+#upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep model | sed 's/  //g'
+#upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep serial | sed 's/  //g'
+#echo
+
+
 sudo lshw -html > /root/reports/$reportname
 #edit ip.add.re.ss and  reporteruser,reporteruserpassword to add your login 
 lftp -e "put /root/reports/$reportname; quit" -u "reporteruser","reporteruserpassword"  sftp://ip.add.re.ss
